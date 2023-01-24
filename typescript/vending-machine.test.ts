0a6f55a04e7f8d25cf10ac88b7a86f2842f93b26
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-01-24 11:20:14 +0100

    Add assert based test

--- typescript/vending-machine.test.ts
+++ typescript/vending-machine.test.ts
@@ -2,10 +2,27 @@ import {describe, it, expect} from 'vitest'
 import VendingMachine from "./vending-machine";
 import VendingMachinePrinter from "./vending-machine-printer";
 
+const coins = {"penny": 1,
+    "nickel": 5,
+    "dime": 10,
+    "quarter": 25}
+
 describe("Vending Machine", () => {
     it("foo", () => {
         let vendingMachine = new VendingMachine()
         let printer = new VendingMachinePrinter(vendingMachine)
         expect(printer.print()).toMatchSnapshot()
     })
+
+    it("accept coins", () => {
+        const machine = new VendingMachine();
+        expect(machine.display === "INSERT COIN")
+
+        machine.insert_coin(coins["nickel"])
+
+        expect(machine.balance).toEqual(5)
+        expect(machine.coins).toEqual([5])
+        expect(machine.display).toEqual("5")
+    })
 })
+
